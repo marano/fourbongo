b0ff6a06fbd397c4d49ba6d3461ada1b8aacd452
marano <thiagomarano@gmail.com>
2011-11-18 14:30:22 -0200

    Refactored home javascript.

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -38,30 +38,105 @@ var geolocation = function () {
   return api;
 }();
 
+var page = function () {
+  api = {};
+  pvt = {
+    thereIsASearchResult: false
+  };
+
+  api.disableSearchNearbyButton = function () { $("#btnSearchNearby").attr("disabled", true); };
+
+  api.enableSearchNearbyButton = function () { $("#btnSearchNearby").attr("disabled", false); };
+
+  api.bindSearchNearbyButton = function (callback) {
+    $("#searchNearby").submit(function (event) {
+      event.preventDefault();
+      callback();
+    });
+  };
+
+  api.bindSearchByNameAndCityButton = function (callback) {
+    $("#searchByNameAndCity").submit(function (event) {
+      event.preventDefault();
+      callback();
+    });
+  };
+
+  pvt.slideContainer = function (callback) {
+    $('#outerContainer').css('overflow', 'hidden');
+    $('#container').animate({left : $(window).width()}, {easing: 'easieEaseInQuint', duration: 1000, complete : callback });
+  };
+
+  api.searchNameInputValue = function () { return $('#inputSearchName').val(); };
+
+  api.searchCityInputValue = function () { return $('#inputSearchCity').val(); };
+
+  api.setSearchCityInputValue = function (value) { $('#inputSearchCity').attr('value', value); };
+
+  pvt.buildResultHtml = function (venues) {
+    var result = $('<div>');
+    if(venues.length > 0) {
+      $(venues).each(function (index, venue) {
+        var link = $('<a>').attr('href', '#').attr('class', 'searchResultItemLink');
+        var div = $('<div>', {class: 'searchResultItem'});
+        var title = $('<span>').attr('class', 'searchResultItemTitle').text(venue.name);
+        result.append(link.append(div.append(title)));
+      });
+    } else {
+      result.attr('id', 'no_search_results').text('OMG, no results!');
+    }
+    return result;
+  };
+
+  pvt.showSpinning = function () { $('#spinning').removeClass('hidden'); };
+
+  pvt.hideSpinning = function () { $('#spinning').addClass('hidden'); };
+
+  api.prepareToSearch = function () {
+    pvt.showSpinning();
+    if(pvt.thereIsASearchResult) {
+      resultContainer = $('#searchResultContainer');
+      resultContainer.animate({top: -resultContainer.height()}, {easing: 'easeOutBounce', duration: 1000});
+    }
+  };
+
+  api.showSearchResult = function (venues) {
+    pvt.hideSpinning();
+    pvt.thereIsASearchResult = true;
+    var html = pvt.buildResultHtml(venues);
+    resultContainer = $('<div>', {id:'searchResultContainer'}).css('position', 'relative').append(html);
+    $('#searchResult').empty().append(resultContainer);
+    resultContainer.css('top', -resultContainer.height());
+    resultContainer.animate({top: 0}, {easing: 'easeOutBounce', duration: 1000});
+
+    $('.searchResultItemLink').each(function (key, linkElement) {
+      var link = $(linkElement);
+      var href = link.attr('href');
+      link.attr('href', '#');
+      link.click(function (event) {
+        event.preventDefault();
+        pvt.slideContainer(function () { window.location = href });
+      });
+    });
+  };
+
+  return api;
+}();
+
 var home = function () {
     var api = {};
 
     var pvt = {
-      thereIsASearchResult: false,
       latitude: null,
       longitude: null,
       geolocationFetched: false
     };
 
     api.initialize = function () {
-      $("#btnSearchNearby").attr("disabled", 'true');
-
+      page.disableSearchNearbyButton();
       pvt.queryGeolocation();
-
-      $("#searchNearby").submit(function (event) {
-        event.preventDefault();
-        pvt.searchNearbyVenues();
-      });
-
-      $("#searchByNameAndCity").submit(function (event) {
-        event.preventDefault();
-        pvt.searchVenuesByNameAndCity();
-      });
+      page.bindSearchNearbyButton(pvt.searchNearbyVenues);
+      page.bindSearchByNameAndCityButton(pvt.searchVenuesByNameAndCity);
     };
 
     pvt.queryGeolocation = function () {
@@ -69,86 +144,36 @@ var home = function () {
         pvt.latitude = latitude;
         pvt.longitude = longitude;
         pvt.geolocationFetched = true;
-        $("#btnSearchNearby").attr("disabled", false);
+        page.enableSearchNearbyButton();
         geolocation.city(latitude, longitude, function (city) {
-          if ($('#inputSearchCity').attr('value') == null || $('#inputSearchCity').attr('value') == '') {
-            $('#inputSearchCity').attr('value', city);
+          if (page.searchCityInputValue() == null || page.searchCityInputValue()  == '') {
+            page.setSearchCityInputValue(city);
           }
         });
       });
     };
     
-    pvt.buildResultHtml = function (venues) {
-      var result = $('<div>');
-      if(venues.length > 0) {
-        $(venues).each(function (index, venue) {
-          var link = $('<a>').attr('href', '#').attr('class', 'searchResultItemLink');
-          var div = $('<div>').attr('class', 'searchResultItem');
-          var title = $('<span>').attr('class', 'searchResultItemTitle').text(venue.name);
-          result.append(link.append(div.append(title)));
-        });
-      } else {
-        result.attr('id', 'no_search_results').text('OMG, no results!');
-      }
-      return result;
-    };
-
     pvt.searchNearbyVenues = function () {
         if (!pvt.geolocationFetched) {
           return;
         }
-        pvt.prepareToSearch();
+        page.prepareToSearch();
         foursquare.search_by_location(pvt.latitude, pvt.longitude, function (venues) {
-          pvt.showSearchResult(pvt.buildResultHtml(venues));
+          page.showSearchResult(venues);
         });
-        return false;
     };
 
     pvt.searchVenuesByNameAndCity = function () {
-      pvt.prepareToSearch();
-      var name = $("#inputSearchName").attr('value');
-      var city = $("#inputSearchCity").attr('value');
+      page.prepareToSearch();
+      var name = page.searchNameInputValue();
+      var city = page.searchCityInputValue();
       if (name == null || name == '' || city == null || city == '') {
         name = '_';
         city = '_';
       }
       foursquare.search_by_name_and_city(name, city, function (venues) {
-        pvt.showSearchResult(pvt.buildResultHtml(venues));
+        page.showSearchResult(venues);
       });
-      return false;
-    };
-
-    pvt.prepareToSearch = function () {
-      $('#spinning').removeClass('hidden');
-      if(!pvt.thereIsASearchResult) {
-        return;
-      }
-      resultContainer = $('#searchResultContainer');
-      resultContainer.animate({top: -resultContainer.height()}, {easing: 'easeOutBounce', duration: 1000});
-    };
-
-    pvt.showSearchResult = function (html) {
-      $('#spinning').addClass('hidden');
-      pvt.thereIsASearchResult = true;
-      resultContainer = $('<div>', {id:'searchResultContainer'}).css('position', 'relative').append(html);
-      $('#searchResult').empty().append(resultContainer);
-      resultContainer.css('top', -resultContainer.height());
-      resultContainer.animate({top: 0}, {easing: 'easeOutBounce', duration: 1000});
-
-      $('.searchResultItemLink').each(function (key, linkElement) {
-          var link = $(linkElement);
-          var href = link.attr('href');
-          link.attr('href', '#');
-          link.click(function () {
-            pvt.slideContainer(function () { window.location = href });
-            return false;
-          });
-      });
-    };
-
-    pvt.slideContainer = function (callback) {
-      $('#outerContainer').css('overflow', 'hidden');
-      $('#container').animate({left : $(window).width()}, {easing: 'easieEaseInQuint', duration: 1000, complete : callback });
     };
 
     return api;
