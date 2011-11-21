75d0cc7a818d3aac44db57d04a23114f51e8e62d
marano <thiagomarano@gmail.com>
2011-11-21 15:21:52 -0200

    Gets foursquare users twitter timeline.

--- public/javascripts/wall.js
+++ public/javascripts/wall.js
@@ -64,12 +64,17 @@ var twitter = function () {
   var api = {};
 
   api.timeline = function (userId, callback) {
+    $.getJSON('https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=' + userId + '&count=50&callback=?', function (data) {
+        var tweets = _(data).map(function (tweet) {
+          return Tweet({id: tweet.id_str, username: tweet.user.screen_name, fullname: tweet.user.name, content: tweet.text, avatar: tweet.user.profile_image_url});
+        });
+        callback(tweets);
+    });
   };
 
   api.byLocation = function (latitude, longitude, callback) {
     $.getJSON('http://search.twitter.com/search.json?geocode=' + latitude + ',' + longitude + ',1.0km&rpp=100&callback=?', function (data) {
-      tweets = [];
-      _(data.results).each(function (tweet) { tweets.push(Tweet({id: tweet.id_str, username: tweet.from_user, fullname: tweet.from_user_name, content: tweet.text, avatar: tweet.profile_image_url})); });
+      var tweets = _(data.results).map(function (tweet) { return Tweet({id: tweet.id_str, username: tweet.from_user, fullname: tweet.from_user_name, content: tweet.text, avatar: tweet.profile_image_url}); });
       callback(tweets);
     });
   };
