4ebe32c73bbe04cec2940cc0540166a883594ec1
marano <thiagomarano@gmail.com>
2012-08-02 00:00:42 -0700

    Instagram can search content based on tag.

--- public/javascripts/instagram.js
+++ public/javascripts/instagram.js
@@ -3,6 +3,25 @@ var instagram = function () {
   var pvt = {
     clientId: 'ef0cec7f983646e4a6809a3d0dbd35bf'
   }
+
+  api.mediaByTag = function (tag, callback) {
+    $.getJSON('https://api.instagram.com/v1/tags/' + tag + '/media/recent?&client_id=' + pvt.clientId + '&callback=?', function (data) {
+      var medias = _(data.data).map(function (media) {
+        var caption;
+        if (media.caption != null) {
+          caption = media.caption.text;
+        }
+        var latitude;
+        var longitude;
+        if (media.location != null) {
+          latitude = media.location.latitude;
+          longitude = media.location.longitude;
+        }
+        return InstagramMedia({id: media.id, username: media.user.username, fullname: media.user.full_name, avatar: media.user.profile_picture, isUpdateByLocation: true, latitude: latitude, longitude: longitude, media: media.images.standard_resolution.url, caption: caption, createdAt: new Date(parseInt(media.created_time) * 1000)});
+      });
+      callback(medias);
+    });
+  };
   
   api.mediaByLocation = function (latitude, longitude, callback) {
     $.getJSON('https://api.instagram.com/v1/media/search?lat=' + latitude + '&lng=' + longitude + '&distance=5000&client_id=' + pvt.clientId + '&callback=?', function (data) {
