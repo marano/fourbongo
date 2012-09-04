7b87dd7dc402a2dccec48fecf0517f18a3560a83
marano <thiagomarano@gmail.com>
2012-09-03 22:15:12 -0700

    Missed identation here.

--- public/javascripts/twitter.js
+++ public/javascripts/twitter.js
@@ -4,10 +4,10 @@ var twitter = function () {
 
   api.timeline = function (userId, callback) {
     $.getJSON('https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=' + userId + '&count=200&callback=?', function (data) {
-        var tweets = _(data).map(function (tweet) {
-          return Tweet({id: tweet.id_str, username: tweet.user.screen_name, fullname: tweet.user.name, content: cheatedUnescape(tweet.text), avatar: tweet.user.profile_image_url, createdAt: new Date(tweet.created_at)});
-        });
-        callback(tweets);
+      var tweets = _(data).map(function (tweet) {
+        return Tweet({id: tweet.id_str, username: tweet.user.screen_name, fullname: tweet.user.name, content: cheatedUnescape(tweet.text), avatar: tweet.user.profile_image_url, createdAt: new Date(tweet.created_at)});
+      });
+      callback(tweets);
     });
   };
 
