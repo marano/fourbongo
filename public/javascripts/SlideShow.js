8a4c172dff9144f8756be208e8a8c045e5debe56
marano <thiagomarano@gmail.com>
2012-10-14 17:21:33 -0700

    Slides are stick to each other for more continues slide effect. Loads content and waits fixed time for media to load before sliding.

--- public/javascripts/SlideShow.js
+++ public/javascripts/SlideShow.js
@@ -1,33 +1,27 @@
 function slideShow(container) {
-  var self = {
-    nextDivContent : undefined,
-    currentDiv : undefined,
-    window : $('<div>', {id:'slideWindow'}).css('overflow', 'hidden').css('width', '100%').css('height', '100%')
-  };
+  var self = {};
+
+  var currentDiv = undefined;
+  var slideWindow = $('<div>', {id:'slideWindow'}).css('overflow', 'hidden').css('width', '100%').css('height', '100%');
 
   self.slide = function (content, callback) {
-    self.nextDivContent = content;
-    if (self.currentDiv === undefined) {
-      self.rollFirst(callback);
-    } else {
-      self.currentDiv.animate({'margin-left' : self.window.width()}, {easing: 'easeInQuint', duration: 1000, complete : function () { self.rollNext(callback); } });
-    }
+    var nextDiv = $('<div>').css('margin-left', -slideWindow.width()).append(content);
+    slideWindow.prepend(nextDiv);
+    setTimeout(function () {
+      if (callback != undefined) { callback(); };
+
+      if (currentDiv != undefined) {
+        currentDiv.animate({'margin-left' : slideWindow.width()}, {easing: 'easeOutQuint', duration: 1000, queue: false, complete : function () {
+          $(this).remove();
+        } });
+      }
+
+      nextDiv.animate({'margin-left' : 0}, {easing: 'easeOutQuint', duration: 1000});
+      currentDiv = nextDiv;
+    }, 1000);
   };
 
-  self.rollNext = function (callback) {
-    self.currentDiv.remove();
-    self.rollFirst(callback);
-  };
-
-  self.rollFirst = function (callback) {
-    self.currentDiv = $('<div>').css('margin-left', -self.window.width());
-    self.window.append(self.currentDiv);
-    self.currentDiv.append(self.nextDivContent);
-    if (callback != undefined) { callback(); };
-    self.currentDiv.animate({'margin-left' : '0px'}, {easing: 'easeOutQuint', duration: 1000});
-  };
-
-  container.append(self.window);
+  container.append(slideWindow);
 
   return self;
 }
