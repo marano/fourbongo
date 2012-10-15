d772c9621a1973c4aaeb1e1f19c923d456b63ac0
marano <thiagomarano@gmail.com>
2012-10-14 18:37:33 -0700

    Make sure we get all recent flickr posts by ordering it by creation date and increase the radios from 5 to 10 kilometers for location search.

--- public/javascripts/flickr.js
+++ public/javascripts/flickr.js
@@ -5,7 +5,7 @@ var flickr = function () {
   };
 
   api.picsByLocation = function (latitude, longitude, callback) {
-    $.getJSON('http://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=' + pvt.apiKey + '&content_type=1&media=photos&per_page=500&lat=' + latitude + '&lon=' + longitude + '&extras=description, license, date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_m, url_z, url_l, url_o&jsoncallback=?', function (data) {
+    $.getJSON('http://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=' + pvt.apiKey + '&sort=date-posted-desc&content_type=1&media=photos&radius=10&per_page=500&lat=' + latitude + '&lon=' + longitude + '&extras=description, license, date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_m, url_z, url_l, url_o&jsoncallback=?', function (data) {
       var pics = _(data.photos.photo).map(function (pic) {
         return FlickrPic({id: pic.id, username: pic.ownername, media: pic.url_z, caption: pic.description._content, createdAt: new Date(parseInt(pic.dateupload) * 1000), avatar: pvt.avatar(pic.iconfarm, pic.iconserver, pic.owner), isUpdateByLocation: true, latitude: pic.latitude, longitude: pic.longitude})
       });
